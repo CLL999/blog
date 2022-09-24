e95737a13f8656630a522897e3e5bec7283b88a0
CLL999 <cirnoqvq@163.com>
2022-09-24 21:01:17 +0800

    fix: 显示docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -32,21 +32,19 @@ const config = {
       'classic',
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        docs: false, 
-        // {
-          // sidebarPath: require.resolve('./sidebars.js'),
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-            // 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        // },
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
         blog: {
-          routeBasePath: '/',
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/CLL999/roast_blog/blog/',
+            'https://github.com/CLL999/roast_blog/blog',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
