197a7113fb9555cbdccd552afee9eec1277dd600
CLL999 <cirnoqvq@163.com>
2022-09-24 21:03:01 +0800

    fix: 编辑博客文件夹路径错误

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -44,7 +44,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/CLL999/roast_blog/blog',
+            'https://github.com/CLL999/roast_blog/blog/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
